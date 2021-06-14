f6ee6d11ad4e5d5dc89e41b5b9cfe990de4b5e2d
jeanlimadev <jeanlima.dev@gmail.com>
2021-06-14 19:33:37 -0300

    add estrutura inicial do menu mobile e animacao do botao

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,14 +1,23 @@
+import { useState } from 'react'
+
 import Link from 'next/link'
 import styles from './Header.module.css'
 
-const Header = ({ children }) => {
+const Header = () => {
+
+  const [menuBurgerActive, setMenuBurgerActive] = useState(false)
+
+  const handleBurger = () => {
+    setMenuBurgerActive(!menuBurgerActive)
+  }
+
   return (
     <>
       <header className={styles.header}>
-        <nav className={styles.nav}>
         <Link href="/">
           <img className={styles.linkLogoHeader} src="/images/logowcruz.png" />
         </Link>
+        <nav className={styles.nav}>
           <ul>
             <li>
               <Link href="/">
@@ -27,8 +36,31 @@ const Header = ({ children }) => {
             </li>
           </ul>
         </nav>
+        <div className={styles.hamburger} onClick={handleBurger}>
+          <div className={`${styles.div1} ${menuBurgerActive && styles.active}`}></div>
+          <div className={`${styles.div2} ${menuBurgerActive && styles.active}`}></div>
+          <div className={`${styles.div3} ${menuBurgerActive && styles.active}`}></div>
+        </div>
+        <div className={`${styles.menuMobile} ${menuBurgerActive && styles.active}`}>
+          <ul>
+              <li>
+                <Link href="/">
+                  <a>Home</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="/about">
+                  <a>Sobre nós</a>
+                </Link>
+              </li>
+              <li>
+                <Link href="#">
+                  <a>Produtos</a>
+                </Link>
+              </li>
+            </ul>
+        </div>
       </header>
-      {children}
     </>
   )
 }
